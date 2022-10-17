bf52b4027b308ea1d22a37e8c7bccbc09096ffc5
leslie <lesliethai@gmail.com>
2022-10-17 00:07:59 -0700

    added css for popup search page

--- scripts.js
+++ scripts.js
@@ -2,17 +2,27 @@
 const app = {};
 
 // selectors
-const menuOpen = document.querySelector(".menuOpen");
-const menuClose = document.querySelector(".menuClose");
-const slideOut = document.querySelector(".slideOut");
+app.menuOpen = document.querySelector(".menuOpen");
+app.menuClose = document.querySelector(".menuClose");
+app.slideOut = document.querySelector(".slideOut");
+app.searchButton = document.querySelector(".fa-magnifying-glass");
 
-menuOpen.addEventListener('click', function(event){
-    event.preventDefault();
-    slideOut.classList.remove("displayNone");
-});
-
-menuClose.addEventListener('click', function(event){
-    event.preventDefault();
-    slideOut.classList.add("displayNone");
+// opens mobile menu on click
+app.menuOpen.addEventListener('click', function(){
+    slide();
 }); 
 
+// closes mobile menu on click
+app.menuClose.addEventListener('click', function(){
+    slide();
+}); 
+
+// function to toggle slide out menu display
+function slide(){
+    app.slideOut.classList.toggle("displayNone"); 
+}
+
+// open searchPage
+app.searchButton.addEventListener('click', function(){
+    document.querySelector(".searchPage").classList.toggle("displayNone");
+})
